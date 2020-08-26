6c6318b1ce0741807a7e4256c5aba23594095fae
sharanram06 <sharanram0006@gmail.com>
2020-08-26 21:25:03 +0530

    Commit 2 - Added questions

---
+++ script.js
@@ -0,0 +1,55 @@
+const qanda = [
+	{
+		que: 'Which Country Has The Most Deaths Due To Covid?',
+		ans: 'USA',
+		opt: [ 'India', 'Russia', 'USA', 'Brazil' ]
+	},
+	{
+		que: 'What do doctors suggest as a cure for coronavirus?',
+		ans: 'A combination of lemon and bicarbonate discovered to be effective in Israel',
+		opt: [ 'Hot water gargle', 'Alkaline foods with pH level above that of coronavirus', 'A combination of lemon and bicarbonate discovered to be effective in Israel', 'There is no cure yet; all of the above are false' ]
+	},
+	{
+		que: 'Many news organisations misreported that a European nation sent a bill of £130 billion to China for COVID-19 damages. Which country was dragged into this??',
+		ans: 'Germany',
+		opt: [ 'Germany', 'Russia', 'USA', 'Brazil' ]
+	},
+	{
+		que: 'How many countries, areas or territories are suffering from novel coronavirus outbreak in the World?',
+		ans: 'More than 200',
+		opt: [ 'More than 50', 'More than 100', 'More than 150', 'More than 200' ]
+	},
+	{
+		que: 'Thailand announced that it has proceeded to test its novel coronavirus vaccine on which animal/bird?',
+		ans: 'Monkeys',
+		opt: [ 'Mokeys', 'Lizards', 'Hens', 'Kites' ]
+	},
+	{
+		que: ' In a study, which cells are found in COVID-19 patients bode well for long term immunity?',
+		ans: 'T-Cell',
+		opt: [ 'Endothelial Cells', 'T-Cell', 'D-Cell', 'P-Cell' ]
+	},
+	{
+		que: 'Name the vaccine that is jointly developed by the German company BioNTech and US pharma giant Pfizer for COVID-19?',
+		ans: 'BNT162',
+		opt: [ 'BNT162', 'PICOVACC', 'Both A and B', ' Neither A nor B' ]
+		
+	},
+	{
+		que: 'Name a clinical trial in which blood is transfused from recovered COVID-19 patients to a coronavirus patient who is in critical condition?',
+		ans: ' Plasma Therapy',
+		opt: [ 'Remdesivir', ' Plasma Therapy', 'Solidarity', 'Hydroxychloroquine' ]
+	},
+	{
+		que: 'What happens to a person suffering from COVID-19?',
+		ans: 'All the above are correct',
+		opt: [ 'Around 80% of the people will require no treatment as such and will recover on their own.', 'Around <20% or a small proportion may need hospitalisation.', 'A very small proportion basically suffering from chronic illness may need admission in an Intensive Care Unit (ICU)', 'All the above are correct' ]
+	},
+	{
+		que: 'In which age group the COVID-19 spreads?',
+		ans: 'All the above are correct',
+
+		opt: [ 'COVID-19 occur in all age groups.', ' Coronavirus infection is mild in children.', 'Older person and persons with pre-existing medical conditions are at high risk to develop serious illness.', ' All the above are correct,' ]
+	}
+];
+
